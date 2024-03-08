e20dda8ccb8499f491dbd02aa8681a9f117517b5
Sagar-Chopra <sagarchopra271@gmail.com>
2024-03-08 20:58:56 +0530

    initial

---
+++ server.js
@@ -0,0 +1,17 @@
+import { app } from "./app.js";
+import { config } from "dotenv";
+import { connectDatabase } from "./config/database.js";
+
+config({
+    path: "./config/config.env"
+})
+
+connectDatabase();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Working Fine</h1>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("server is running on port " + process.env.PORT);
+})
